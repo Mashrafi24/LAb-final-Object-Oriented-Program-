b68810c3d14d523f28c49bd1abd2383b48a037a9
Md Mashrafi Rahman <mashrafirahman2024@gmail.com>
2026-01-14 16:01:17 +0600

    Update Write a function named reverse.java

--- Write a function named reverse.java
+++ Write a function named reverse.java
@@ -1,1 +1,24 @@
 
+public class ReverseArrayExample {
+
+    public static void reverse(double[] arr) {
+        int i = 0, j = arr.length - 1;
+        while (i < j) {
+            double temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        double[] data = {5.8, 2.6, 9.0, 3.4, 7.1};
+
+        reverse(data);
+
+        for (double x : data) {
+            System.out.print(x + " ");
+        }
+    }
+}
